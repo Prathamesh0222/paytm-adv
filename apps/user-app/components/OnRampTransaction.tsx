cad8a55fc60ae63cebf30a8f47f6443942cd506d
prathameshpimpalkar <prathameshpimpalkar2002@gmail.com>
2024-07-26 10:06:30 +0530

    Added svg for txn status

--- apps/user-app/components/OnRampTransaction.tsx
+++ apps/user-app/components/OnRampTransaction.tsx
@@ -1,13 +1,66 @@
 import { Card } from "@repo/ui/card";
 
-const getStatement = (status: string) => {
-  if (status === "success") {
-    return "Received INR";
-  } else if (status === "failed") {
-    return "Failed";
-  } else {
-    return "To be Received INR";
-  }
+const checkStatusIcon = (status: string) => {
+  if (status === "processing")
+    return (
+      <div className="flex text-gray-500 font-bold">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth="1.5"
+          stroke="currentColor"
+          className="size-5"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99"
+          />
+        </svg>
+        <div className="pl-1">Processing</div>
+      </div>
+    );
+  else if (status === "success")
+    return (
+      <div className="flex text-red-700 font-bold">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth="1.5"
+          stroke="currentColor"
+          className="size-5"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M6 18 18 6M6 6l12 12"
+          />
+        </svg>
+        <div className="pl-1">Failed</div>
+      </div>
+    );
+  else
+    return (
+      <div className="flex text-green-500 font-bold">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth="1.5"
+          stroke="currentColor"
+          className="size-5"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="m4.5 12.75 6 6 9-13.5"
+          />
+        </svg>
+        <div className="pl-1">Received</div>
+      </div>
+    );
 };
 
 export const OnRampTransaction = ({
@@ -35,8 +88,8 @@ export const OnRampTransaction = ({
         {transactions.map((t) => (
           <div className="flex justify-between">
             <div>
-              <div className="text-sm">{getStatement(t.status)}</div>
-              <div className="text-slate-600 text-xs">
+              <div className="text-sm">{checkStatusIcon(t.status)}</div>
+              <div className="text-slate-600 text-xs pl-1.5 font-semibold">
                 {t.time.toDateString()}
               </div>
             </div>
